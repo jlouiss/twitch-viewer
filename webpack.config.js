4505ade9350ef39b46a721407b3367e91e667a49
Jean Louis Salbego <jlsalbego@gmail.com>
2017-02-11 00:21:25 +0100

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/scripts.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname)
+  },
+  // devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] }
+        }],
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.sass$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+}
